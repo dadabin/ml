4ed23229730f6228a53b56044bbfe3eb592a5b0c
barrysun <1096490965@qq.com>
2015-02-13 10:58:47 +0800

    GifProcessor

--- spider/src/main/java/com/spider/down/gif/GifProcessor.java
+++ spider/src/main/java/com/spider/down/gif/GifProcessor.java
@@ -1,7 +1,29 @@
 package com.spider.down.gif;
 
+import com.spider.down.util.FileProcessor;
+
 /**
  * Created by rimi on 2015/2/13.
  */
-public class GifProcessor {
+public class GifProcessor implements Runnable {
+
+    private String imgName;
+    private String imgUrl;
+
+    public GifProcessor(String name,String url){
+        this.imgName =name;
+        this.imgUrl=url;
+    }
+
+    public void run(){
+        FileProcessor fp=new FileProcessor(this.imgName,this.imgUrl);
+        try{
+            System.out.println("下载保存图片URL:"+this.imgUrl);
+            fp.saveGif();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ spider/src/main/java/com/spider/down/gif/GifSpider.java
@@ -0,0 +1,17 @@
+package com.spider.down.gif;
+
+/**
+ * Created by rimi on 2015/2/13.
+ */
+public class GifSpider implements Runnable {
+
+    volatile boolean isRunning = true;
+
+
+
+
+    public void run(){
+
+    }
+
+}
